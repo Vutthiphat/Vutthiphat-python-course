181b514644b6ba3eb29a8c30fd4c0b84d53fb95f
Vutthiphat Nithipakorn <vutthiphat.n@ku.th>
2026-09-25 14:23:38 +0700

    asf

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -1,24 +1,33 @@
-"""
-    สร้าง class Rectangle โดยกำหนดให้
-    - มี attribute ชื่อ length และ width ที่เก็บข้อมูลความยาวและความกว้างของสี่เหลี่ยม
-    - มี method ชื่อ get_area() ที่คืนค่าพื้นที่ของสี่เหลี่ยม
-    - มี method ชื่อ get_perimeter() ที่คืนค่ารอบรูปของสี่เหลี่ยม
-"""
-
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
 
-    # Method to get the area
+    
     def get_area(self):
-        pass
+        return self.length * self.width
 
-    # Method to get the perimeter
+    
     def get_perimeter(self):
-        pass
+        return 2 * (self.length + self.width)
 
 
 rect = Rectangle(10, 5)
-print(rect.get_area())       # Should print 50
-print(rect.get_perimeter())  # Should print 30
+print(rect.get_area())       
+print(rect.get_perimeter())  
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return 3.14 * (self.radius ** 2)
+
+    def get_circumference(self):
+        return 2 * 3.14 * self.radius
+
+
+mycircle = Circle(10)
+print(mycircle.get_area())          
+print(mycircle.get_circumference()) 
---
+++ labs/week10-lab/kan.py
@@ -0,0 +1,8 @@
+text = input("Enter text: ")
+
+reverse = text[::-1]
+
+if text == reverse:
+    print("Yes")
+else:
+    print("No")
